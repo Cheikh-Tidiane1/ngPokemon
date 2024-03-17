8d08d48cf897c6a2ff70a6574b9911b723e4e2c6
cheikh tidiane <cheikh951812@gmail.com>
2024-03-17 04:59:59 +0100

    Display: PokemonList

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PokeListService } from './poke-list.service';
+import { Pokemon } from './pokemon';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'ngPokemon';
+export class AppComponent implements OnInit {
+  val: string;
+  pokemonList: Pokemon[];
+  constructor(private pokeService: PokeListService) {}
+  ngOnInit(): void {
+    this.pokemonList = this.pokeService.pokemons;
+  }
 }
