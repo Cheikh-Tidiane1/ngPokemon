5e091ac448305e11aa985cd86e53b275f7af3e44
cheikh tidiane <cheikh951812@gmail.com>
2024-03-18 22:19:21 +0100

    detailPokemon: route updtae

--- src/app/detail-pokemon/detail-pokemon.component.ts
+++ src/app/detail-pokemon/detail-pokemon.component.ts
@@ -1,21 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { PokeListService } from '../poke-list.service';
 import { Pokemon } from '../pokemon';
 import { PokemonTypeColorPipe } from '../pokemon-type-color.pipe';
 import { DatePipe } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 
 @Component({
   selector: 'app-detail-pokemon',
   standalone: true,
-  imports: [PokemonTypeColorPipe,DatePipe],
+  imports: [PokemonTypeColorPipe,DatePipe,RouterLink],
   templateUrl: './detail-pokemon.component.html',
   styleUrl: './detail-pokemon.component.css',
 })
 export class DetailPokemonComponent implements OnInit {
   pokemon: Pokemon 
-  constructor(private route: ActivatedRoute, private listService: PokeListService){}
+  constructor(private route: ActivatedRoute, private listService: PokeListService,private router: Router){}
 
   ngOnInit(): void {
     const idPokemon = this.route.snapshot.params['id']
@@ -23,4 +24,8 @@ export class DetailPokemonComponent implements OnInit {
       this.pokemon = this.listService.getPokemon(idPokemon)!
     }
   }
+
+  // goToPokemon(){
+  //   this.router.navigate(['pokemons'])
+  // }
 }
--- src/app/list-pokemon/list-pokemon.component.ts
+++ src/app/list-pokemon/list-pokemon.component.ts
@@ -4,17 +4,23 @@ import { BorderCardDirective } from '../border-card.directive';
 import { Pokemon } from '../pokemon';
 import { DatePipe } from '@angular/common';
 import { PokemonTypeColorPipe } from '../pokemon-type-color.pipe';
+import { Router } from '@angular/router';
+import { NgStyle } from '@angular/common';
 @Component({
   selector: 'app-list-pokemon',
   standalone: true,
-  imports: [BorderCardDirective, DatePipe, PokemonTypeColorPipe],
+  imports: [BorderCardDirective, DatePipe, PokemonTypeColorPipe, NgStyle],
   templateUrl: './list-pokemon.component.html',
   styleUrl: './list-pokemon.component.css',
 })
 export class ListPokemonComponent implements OnInit {
   pokemonList: Pokemon[];
-  constructor(private pokeService: PokeListService) {}
+  constructor(private pokeService: PokeListService, private router: Router) {}
   ngOnInit(): void {
     this.pokemonList = this.pokeService.pokemons;
+  }
+
+  goToPokemon(pokemon: Pokemon) {
+    this.router.navigate(['pokemons', pokemon.id]);
   }
 }
