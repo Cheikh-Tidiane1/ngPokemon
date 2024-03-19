988801d10d90768728e94ab0d156323e0d3005d0
cheikh tidiane <cheikh951812@gmail.com>
2024-03-20 00:45:04 +0100

    form-Pokemon: update

---
+++ src/app/edit-pokemon/edit-pokemon.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PokeListService } from '../poke-list.service';
+import { Pokemon } from '../pokemon';
+import { PokemonFormComponent } from "../pokemon-form/pokemon-form.component";
+
+@Component({
+    selector: 'app-edit-pokemon',
+    standalone: true,
+    templateUrl: './edit-pokemon.component.html',
+    styleUrl: './edit-pokemon.component.css',
+    imports: [PokemonFormComponent]
+})
+export class EditPokemonComponent implements OnInit {
+  pokemon: Pokemon;
+
+  constructor(
+    private route: ActivatedRoute,
+    private listService: PokeListService
+  ) {}
+
+  ngOnInit(): void {
+    const pokemonId = this.route.snapshot.params['id'];
+    if (pokemonId) {
+      this.pokemon = this.listService.getPokemon(pokemonId)!;
+    }
+  }
+}
--- src/app/pokemon-form/pokemon-form.component.ts
+++ src/app/pokemon-form/pokemon-form.component.ts
@@ -1,12 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { PokeListService } from '../poke-list.service';
+import { Pokemon } from '../pokemon';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { PokemonTypeColorPipe } from '../pokemon-type-color.pipe';
 
 @Component({
   selector: 'app-pokemon-form',
   standalone: true,
-  imports: [],
   templateUrl: './pokemon-form.component.html',
-  styleUrl: './pokemon-form.component.css'
+  styleUrl: './pokemon-form.component.css',
+  imports: [FormsModule, PokemonTypeColorPipe],
 })
-export class PokemonFormComponent {
+export class PokemonFormComponent implements OnInit {
+  @Input() pokemon: Pokemon;
+  types: string[];
+  constructor(
+    private pokemonService: PokeListService,
+    private router: Router
+  ) {}
 
+  ngOnInit() {
+    this.types = this.pokemonService.getPokemonTypeList();
+  }
+
+  hasType(type: string): boolean {
+    return this.pokemon.types.includes(type);
+  }
+
+  selectType(event: Event, type: string) {
+    const isChecked = (event.target as HTMLInputElement).checked;
+    if (isChecked) {
+      this.pokemon.types.push(type);
+    } else {
+      const index = this.pokemon.types.indexOf(type);
+      this.pokemon.types.slice(index, 1);
+    }
+  }
+
+  onSubmit() {
+    console.log('submit ok');
+    this.router.navigate(['/pokemons', this.pokemon.id]);
+  }
+
+  isTypesValid(type: string): boolean {
+    if (this.pokemon.types.length == 1 && this.hasType(type)) {
+      return false;
+    }
+
+    if (this.pokemon.types.length > 2 && !this.hasType(type)) {
+      return false;
+    }
+
+    return true;
+  }
 }
