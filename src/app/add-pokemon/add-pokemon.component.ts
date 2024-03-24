efbfc3fe0bdf909e348c6a8222c2abde5c2328b4
cheikh tidiane <cheikh951812@gmail.com>
2024-03-24 14:49:37 +0100

    Component: addPokemon

---
+++ src/app/add-pokemon/add-pokemon.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-pokemon',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-pokemon.component.html',
+  styleUrl: './add-pokemon.component.css'
+})
+export class AddPokemonComponent {
+
+}
